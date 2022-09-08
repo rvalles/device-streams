e3b374af2fe81a663992ce4e4dc592a94c3fc9b7
Roc Vallès Domènech <vallesroc@gmail.com>
2022-09-09 07:41:45 +0900

    Format headers with clang-format.

--- devices.h
+++ devices.h
@@ -1,10 +1,10 @@
-/* -------------------------------------------------- 
+/* --------------------------------------------------
  |  NAME
  |    devices
  |  PURPOSE
  |    provide simple routines and access to an exec device.
  |  NOTES
- | 
+ |
  |  COPYRIGHT
  |    Copyright (C) 1993  Christian E. Hopps
  |
@@ -21,31 +21,30 @@
  |    You should have received a copy of the GNU General Public License
  |    along with this program; if not, write to the Free Software
  |    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- |    
+ |
  |  HISTORY
  |    chopps - Oct 9, 1993: Created.
  +--------------------------------------------------- */
 
-#if ! defined (_DEVICES_H)
+#if !defined(_DEVICES_H)
 #define _DEVICES_H
 #include "util.h"
-#include <exec/ports.h>
-#include <exec/io.h>
 #include <exec/devices.h>
+#include <exec/io.h>
+#include <exec/ports.h>
 
 struct device_data {
     struct MsgPort *port;
     struct IORequest *io;
-    char  *name;
-    ulong  unit;
-    ulong  flags;
-    int    open;
-    
+    char *name;
+    ulong unit;
+    ulong flags;
+    int open;
 };
 
-struct device_data * init_device (char *name, ulong unit, ulong flags, ulong iosize);
-int open_device (struct device_data *dd);
-void close_device (struct device_data *dd);
-void free_device (struct device_data *dd);
+struct device_data *init_device(char *name, ulong unit, ulong flags, ulong iosize);
+int open_device(struct device_data *dd);
+void close_device(struct device_data *dd);
+void free_device(struct device_data *dd);
 
 #endif /* _DEVICES_H */
--- util.h
+++ util.h
@@ -1,10 +1,10 @@
-/* -------------------------------------------------- 
+/* --------------------------------------------------
  |  NAME
  |    util
  |  PURPOSE
  |    header for util.c
  |  NOTES
- | 
+ |
  |  COPYRIGHT
  |    Copyright (C) 1993  Christian E. Hopps
  |
@@ -21,20 +21,20 @@
  |    You should have received a copy of the GNU General Public License
  |    along with this program; if not, write to the Free Software
  |    Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- |    
+ |
  |  HISTORY
  |    chopps - Oct 9, 1993: Created.
  +--------------------------------------------------- */
 
-#if ! defined (_UTIL_H)
+#if !defined(_UTIL_H)
 #define _UTIL_H
 
-#include <exec/types.h>
-#include <exec/memory.h>
-#include <exec/nodes.h>
-#include <exec/lists.h>
 #include <dos/dos.h>
 #include <dos/dosextens.h>
+#include <exec/lists.h>
+#include <exec/memory.h>
+#include <exec/nodes.h>
+#include <exec/types.h>
 
 #include <clib/exec_protos.h>
 #include <pragmas/exec_pragmas.h>
@@ -49,19 +49,19 @@ extern ulong opt_debug;
 extern ulong opt_verbose;
 
 /* Macros */
-#define copy_string(x) alloc_string (x)
-#define ptrfrom(st,fl,p) ((st *)(((char *)p) - (offsetof (st,fl))))
-#define valid_mem(m) (TypeOfMem (m))
-#define megs(x) ((x)/(1024*1024))
-#define tenths_of_a_meg(x) ((10*(((x)/1024) % 1024))/1024)
+#define copy_string(x) alloc_string(x)
+#define ptrfrom(st, fl, p) ((st *)(((char *)p) - (offsetof(st, fl))))
+#define valid_mem(m) (TypeOfMem(m))
+#define megs(x) ((x) / (1024 * 1024))
+#define tenths_of_a_meg(x) ((10 * (((x) / 1024) % 1024)) / 1024)
 
 extern FILE *mout;
 extern FILE *min;
 
-#if defined (DEBUG_ENABLED_VERSION)
+#if defined(DEBUG_ENABLED_VERSION)
 #define D(x) x
 #else
 #define D(x)
-#endif				   
+#endif
 
 #endif /* _UTIL_H */
