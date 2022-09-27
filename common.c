d20306fb7fb597400fe6216165fdd2037f7de9da
Roc Vallès Domènech <vallesroc@gmail.com>
2022-09-28 06:11:01 +0900

    common.c: check_values() safeguard for 32bit overflow.

--- common.c
+++ common.c
@@ -1,6 +1,8 @@
 #include "util.h"
 
 int check_values(Partition *p, ulong st, ulong end, int exp) {
+    unsigned long long offset;
+    unsigned long long bytes;
     if (st > end) {
         message("error: Your end block [%lu] is less than your start block [%lu]!\n", st, end);
         return (0);
@@ -19,6 +21,12 @@ int check_values(Partition *p, ulong st, ulong end, int exp) {
                     "       same block numbers as the partition's).");
             return (0);
         }
+    }
+    offset = (unsigned long long)st * p->block_size;
+    bytes = (unsigned long long)(end-st) * p->block_size;
+    if (offset >= 1ULL << 32 || offset + bytes - 1 >= 1ULL << 32) {
+        message("error: Your block range requires 64bit I/O support.");
+        return (0);
     }
     return (1);
 }
